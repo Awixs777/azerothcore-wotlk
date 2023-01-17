56922993b4c006f3c5194cddfdcdccbf08bfb98e
Awixs777 <47874183+Awixs777@users.noreply.github.com>
2023-01-17 10:52:35 +0300

    fix buff solo

--- src/server/scripts/Custom/inst_buff.cpp
+++ src/server/scripts/Custom/inst_buff.cpp
@@ -9,13 +9,18 @@ public:
     /* бафф инст*/
     void OnUpdateZone(Player* player, uint32 newZone, uint32 newArea)
     {
-        // Проверка в ТМ
-        if (!player->GetGroup() && (player->GetMapId() == 585 || player->GetMapId() == 550 || player->GetMapId() == 552 || player->GetMapId() == 553 || player->GetMapId() == 554 || player->GetMapId() == 601 || player->GetMapId() == 533 || player->GetMapId() == 555 || player->GetMapId() == 632 || player->GetMapId() == 658 || player->GetMapId() == 668))
+        // проверка входа
+        if (!player->GetGroup() && (player->GetMapId() == 585 || player->GetMapId() == 550 || player->GetMapId() == 552
+            || player->GetMapId() == 553 || player->GetMapId() == 554 || player->GetMapId() == 601
+            || player->GetMapId() == 533 || player->GetMapId() == 555 || player->GetMapId() == 632
+            || player->GetMapId() == 658 || player->GetMapId() == 668 || player->GetMapId() == 602
+            || player->GetMapId() == 615 || player->GetMapId() == 565))
         {
             player->CastSpell(player, 90005);
             ChatHandler(player->GetSession()).PSendSysMessage("|cffff6060[Buff-Solo]:|rВы получили бафф увеличения характеристик.");
         }
 
+        // проверка выхода, сброс бафа
         if (player->GetMapId() == 1 || player->GetMapId() == 0 || player->GetMapId() == 571 || player->GetMapId() == 530 || player->GetGroup())
             player->RemoveAurasDueToSpell(90005);
     }

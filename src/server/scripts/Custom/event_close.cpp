9dd65cd7cb51cb909a93764e0c68f23984adb0b9
Awixs777 <gfy2302@gmail.com>
2025-03-05 11:39:00 +0300

    ZoneCheck

--- src/server/scripts/Custom/event_close.cpp
+++ src/server/scripts/Custom/event_close.cpp
@@ -25,13 +25,14 @@ public:
 
         // AOE LOOT
         if (!player->GetGroup() && (player->GetMapId() == 624 || player->GetAreaId() == 35 || player->GetZoneId() == 2817 ||
-            player->GetAreaId() == 279 || player->GetAreaId() == 3749))
+            player->GetAreaId() == 279 || player->GetAreaId() == 3749 || player->GetMapId() == 557 || player->GetMapId() == 556
+            || player->GetMapId() == 544 || player->GetMapId() == 558))
         {
-            ChatHandler(player->GetSession()).PSendSysMessage("|cffff6060[AutoLoot]:|r\nВ данной локации работает Соло [AutoLoot]|r");
+            ChatHandler(player->GetSession()).PSendSysMessage("|cffff6060[AutoLoot]:|r В данной локации работает SOLO автолут.|r");
         }
-
+        
         // Кач
-        if (player->GetZoneId() == 557)
+        if (player->GetAreaId() == 3792)
         {
             ChatHandler(player->GetSession()).PSendSysMessage("|cffff6060[Путеводитель]:|rВы не получаете опыта за убийство - это нормально.\nПосле выполнения квеста, Вы, получите предмет для повышения уровня.!|r");
         }
@@ -41,6 +42,35 @@ public:
         {
             player->TeleportTo(571, 6236.229980f, 5768.240234f, -5.373631f, 0.736042f);
             ChatHandler(player->GetSession()).PSendSysMessage("|cffff6060[Доступ Запрещен]:|r Необходим 230-й уровень!|r");
+        }
+
+        // Повелитель
+        if (player->GetAreaId() == 3615 && player->GetLevel() < 230 && !player->IsGameMaster())
+        {
+            player->TeleportTo(571, 6236.229980f, 5768.240234f, -5.373631f, 0.736042f);
+            ChatHandler(player->GetSession()).PSendSysMessage("|cffff6060[Доступ Запрещен]:|r Необходим 230-й уровень!|r");
+        }
+
+        // Помощь Стратхольму
+        if (player->GetAreaId() == 4100)
+        {
+            // Список квестов
+            std::array<uint32_t, 7> questIds = { 20551, 20057, 20058, 20059, 20060, 20061, 20062 };
+
+            // Проходим по всем квестам
+            for (uint32_t questId : questIds)
+            {
+                Quest const* quest = sObjectMgr->GetQuestTemplate(questId);
+
+                // Проверка наличия квеста и его статуса
+                if (quest && (!player->GetQuestRewardStatus(questId) || player->IsActiveQuest(questId)))
+                {
+                    player->AddQuest(quest, player);
+                }
+            }
+
+            // Сообщение об успешной выдаче квестов
+            ChatHandler(player->GetSession()).PSendSysMessage("|cffff6060[Доступ ТМ]:|r Выданы квесты необходимые для получения доступа к ТМ|r");
         }
 	}
 };
